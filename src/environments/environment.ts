5d31e0baa9006f44d11c62dd89f1045b4c1ad9db
iam-dha <iamdha0706@gmail.com>
2025-06-09 14:19:27 +0700

    change api endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://54.226.168.204:8080/api/v1' // Update this with your actual API URL
+  apiUrl: 'http://54.87.12.126:8080/api/v1' // Update this with your actual API URL
 }; 
